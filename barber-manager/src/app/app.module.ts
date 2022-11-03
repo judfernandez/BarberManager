8b88dd2dbe7e5431056edcad257e1e7439fa5d43
Juan David Fernández Moreno <judfernandezmo@gmail.com>
2022-11-03 11:50:14 -0500

    routing and material design ready

--- barber-manager/src/app/app.module.ts
+++ barber-manager/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BarbersComponent } from './barbers/barbers.component';
+import { RegisterComponent } from './register/register.component';
+import { RequestComponent } from './request/request.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ErrorComponent,
+    CalendarComponent,
+    DashboardComponent,
+    BarbersComponent,
+    RegisterComponent,
+    RequestComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
